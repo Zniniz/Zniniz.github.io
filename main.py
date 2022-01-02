32d4bbfccc362e994e0e8159071604d59bd00fb4
Zniniz <shaheerqwi@gmail.com>
2022-01-02 15:40:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-for _ in range(100):
-    print("Hello World")
+import turtle
+
+colours = [
+    "yellow", "cyan", "red", "blue", "magenta", "white", "green"
+]
+
+cycles = 6
+size = 50
+offset = 10
+window = turtle.Screen()
+window.bgcolor("black")
+turtle.pensize(0.5)
+turtle.speed(0.5)
+
+for i in range(cycles):
+    for c in colours:
+        turtle.color(c)
+        turtle.circle(size)
+        turtle.left(offset)
+
+turtle.hideturtle()            
